67fb8f7aacff182f41d8bff6a164ebbcd725bdf4
Jackmt9 <jackmt9@gmail.com>
2020-03-24 10:16:53 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(string)
+        anagram = []
+        string.each do |word|
+            if word.split(//).sort == @word.split(//).sort
+                anagram << word
+            end
+        end
+        return anagram
+    end
+end
